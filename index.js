5e4e777bc06e2405f84eeda96256562a0c652cda
omer-zauber <omer.zauber@gmail.com>
2018-02-18 11:44:11 +0200

    updated index.js

--- index.js
+++ index.js
@@ -3,16 +3,16 @@ const mongoose = require('mongoose');
 const cookieSession = require('cookie-session');
 const passport = require('passport');
 const bodyParser = require('body-parser');
-
 const keys = require('./config/keys');
 require('./models/User');
+
 require('./services/passport');
 
-mongoose.connect(keys.mongoURI).then(() => {
-	console.log('connected to mongoDB');
-});
+mongoose.Promise = global.Promise;
+mongoose.connect(keys.mongoURI);
 
 const app = express();
+
 app.use(bodyParser.json());
 app.use(
 	cookieSession({
@@ -26,16 +26,19 @@ app.use(passport.session());
 require('./routes/authRoutes')(app);
 require('./routes/billingRoutes')(app);
 
-if (process.env.NODE_ENV === 'production') {
-	app.use(express.static('client/build'));
-	// serves main.js/main.css
 
+if (process.env.NODE_ENV === 'production') {
+	// Express will serve up production assets
+	// like our main.js file, or main.css file!
+	app.use(express.static('client/build'));
+
+	// Express will serve up the index.html file
+	// if it doesn't recognize the route
 	const path = require('path');
 	app.get('*', (req, res) => {
-		res.sendFile(path.resolce(__dirname, 'client', 'build', 'index.html'));
+		res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
 	});
-	//serves index.html doe paths server doesnt recognize.
 }
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log('server listens on port', PORT));
+app.listen(PORT);
