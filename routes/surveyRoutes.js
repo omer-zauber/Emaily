37089c8609ebfb9eed86369c930f020ee6a38506
omer-zauber <omer.zauber@gmail.com>
2018-02-19 13:11:37 +0200

    added mailer services

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -1,23 +1,25 @@
-const mongoose = require('mongoose');
-const requireLogin = require('../middlewares/requireLogin');
-const requireCredits = require('../middlewares/requireCredits');
+const mongoose = require("mongoose");
+const requireLogin = require("../middlewares/requireLogin");
+const requireCredits = require("../middlewares/requireCredits");
+const Mailer = require("../services/Mailer");
+const surveyTemplate = require("../services/emailTemplates/surveyTemplate.js");
 
-const Survey = mongoose.model('surveys');
+const Survey = mongoose.model("surveys");
 
 module.exports = app => {
-	app.post('/api/surveys', requireLogin, requireCredits, (req, res) => {
-		const { title, subject, body, recipients } = req.body;
+  app.post("/api/surveys", requireLogin, requireCredits, (req, res) => {
+    const { title, subject, body, recipients } = req.body;
 
-		const survey = new Survey({
-			title,
-			subject,
-			body,
-      recipients: recipients
-        .split(',')
-        .map(email => ({ email: email.trim() })
-      ),
-			_user: req.user.id,
-			dateSent: Date.now(),
-		});
-	});
+    const survey = new Survey({
+      title,
+      subject,
+      body,
+      recipients: recipients.split(",").map(email => ({ email: email.trim() })),
+      _user: req.user.id,
+      dateSent: Date.now()
+    });
+
+    const mailer = new Mailer(survey, surveyTemplate(survey));
+    mailer.send();
+  });
 };
